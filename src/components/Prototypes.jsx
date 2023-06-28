27df281698077ccdb6ef634b90d865a0685f1e6d
jjgodcom <jjgodcom1@gmail.com>
2023-06-29 04:02:59 +0900

    상품 레이아웃 추가

--- src/components/Prototypes.jsx
+++ src/components/Prototypes.jsx
@@ -138,7 +138,43 @@ export default function Prototypes() {
   return (
     <main>
       <div className="prototypes">
-        상품 리스트
+        {prototypes.map(prototype => {
+          const {id, thumbnail, title, price, desc, pieUrl} = prototype;
+          return (
+            <div className="prototype" key={id}>
+              <a href={pieUrl} target="_blank">
+                <div 
+                  style={{
+                    padding:"25px 0 33px 0"
+                  }}
+                >
+                  <video 
+                    className="prototype__artwork prototype__edit"
+                    src={thumbnail}
+                    style={{
+                      objectFit:"contain"
+                    }}
+                    autoPlay 
+                    loop 
+                    playsInline>
+                  </video>
+                </div>
+              </a>
+
+              <div className="prototype__body">
+                <div className="prototype__title">
+                  <div className="btn btn--primary float--right">
+                    <i className="icon icon--plus" />
+                  </div>
+                  {title}
+                </div>
+                <p className="prototype__price">$ {price}</p>
+                <p className="prototype__desc">{desc}</p>
+              </div>
+
+            </div>
+          );
+        })}
       </div>
     </main>
   );

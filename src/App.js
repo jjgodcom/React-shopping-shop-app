f04172c44f04e51103587a28b60ee2788074ad4b
jjgodcom <jjgodcom1@gmail.com>
2023-06-29 04:56:51 +0900

    중간 push

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import Header from './components/Header'
 import Prototypes from './components/Prototypes'
 import Orders from './components/Orders'
 import Footer from './components/Footer'
+import AppStateProvider from './providers/AppStateContext';
 
 function App() {
   return (
-    <>
+    <AppStateProvider>
       <Header></Header>
       <div className="container">
         <Prototypes></Prototypes>
         <Orders></Orders>
         <Footer></Footer>
       </div>
-    </>
+    </AppStateProvider>
   );
 }
 
